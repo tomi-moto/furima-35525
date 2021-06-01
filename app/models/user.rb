8bc96f518d23dbca0a618ba829225158710308c6
tomi-moto <tomimoto1720365@gmail.com>
2021-06-01 17:30:52 +0900

    修正００２

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birth_date
-    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥々ー]/} do
       validates :first_name
       validates :last_name
     end
