5b44f7229038ee71c1b5e80b8a9b43816f41e113
tomi-moto <tomimoto1720365@gmail.com>
2021-06-04 17:11:12 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,14 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
   private
 
   def item_params
