062193edb58c697bc790b83f75026e835cd78c89
tomi-moto <tomimoto1720365@gmail.com>
2021-06-04 16:32:04 +0900

    second commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
 
   def index
     @items = Item.order("created_at DESC")
@@ -22,10 +25,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
   private
 
   def item_params
     params.require(:item).permit(:title, :price, :introduction, :user, :category_id, :state_id, :fee_id, :prefecture_id, :days_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
+  def move_to_index
+    redirect_to root_path unless current_user == @item.user
   end
 
 end
