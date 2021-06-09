1b772d69296b7a43710f4c3b8bea1e5cdb1bc3f9
tomi-moto <tomimoto1720365@gmail.com>
2021-06-09 15:54:08 +0900

    修正００２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,7 +38,6 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:item_id])
     redirect_to root_path if current_user == @item.user
   end
 
--- app/models/order_user.rb
+++ app/models/order_user.rb
@@ -1,6 +1,6 @@
 class OrderUser
   include ActiveModel::Model
-  attr_accessor :post_num, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token, :purchase_record_id
+  attr_accessor :post_num, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
     ## order_userのバリデーション
     with_options presence: true do
         validates :post_num, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
